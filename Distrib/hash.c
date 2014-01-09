9d38d578cfa2126a5c100bdd0320aa681a412838
lykin06 <lykin06@gmail.com>
2014-01-09 11:42:34 +0100

    deplacement

--- Distrib/hash.c
+++ Distrib/hash.c
@@ -1,2 +1,11 @@
-#include "table.h"
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <malloc.h>
+#include "correct.h"
+#include "corpus.h"
+#include "hash.h"
+
+void hash_table_create(void){
+	strtok("Iliad.txt"," ");
+}
