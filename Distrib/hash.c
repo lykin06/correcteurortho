a78c5f3fa03a0abca4d00890a064915428938305
user <user@zxxx.(none)>
2014-01-09 12:15:45 +0100

    et TOK

--- Distrib/hash.c
+++ Distrib/hash.c
@@ -1,2 +1,16 @@
-#include "table.h"
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <malloc.h>
+#include "correct.h"
+#include "corpus.h"
+#include "hash.h"
+
+void hash_table_create(void){
+	
+	char toto[] ="BONJOUR TOTO FOO";
+	printf("%s\n",strtok(toto," "));
+	printf("%s\n",strtok(NULL," "));
+
+
+}
